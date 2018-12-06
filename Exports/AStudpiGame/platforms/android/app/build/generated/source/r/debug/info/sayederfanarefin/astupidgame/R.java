700351807a3f03b44f0e43b5e2306c740ec4b482
Sayed Erfan Arefin <erfanjordison@gmail.com>
2018-12-06 22:15:05 +0100

    so far working

--- Exports/AStudpiGame/platforms/android/app/build/generated/source/r/debug/info/sayederfanarefin/astupidgame/R.java
+++ Exports/AStudpiGame/platforms/android/app/build/generated/source/r/debug/info/sayederfanarefin/astupidgame/R.java
@@ -12,7 +12,8 @@ public final class R {
     public static final int ic_launcher_background=0x7f010000;
   }
   public static final class drawable {
-    public static final int screen=0x7f020000;
+    public static final int ic_launcher_background=0x7f020000;
+    public static final int screen=0x7f020001;
   }
   public static final class mipmap {
     public static final int ic_launcher=0x7f030000;
