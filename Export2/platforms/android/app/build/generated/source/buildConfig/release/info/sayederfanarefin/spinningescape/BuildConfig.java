e6147c41aa72acddffb5dbb40ef5239042b3771a
Sayed Erfan Arefin <erfanjordison@gmail.com>
2018-12-16 23:59:24 +0100

    launched

---
+++ Export2/platforms/android/app/build/generated/source/buildConfig/release/info/sayederfanarefin/spinningescape/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package info.sayederfanarefin.spinningescape;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "info.sayederfanarefin.spinningescape";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 10000;
+  public static final String VERSION_NAME = "1.0.0.0";
+}
